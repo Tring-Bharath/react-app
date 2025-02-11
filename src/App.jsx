0574520bbc6a07efec70895d7f8218619104fb03
barath <barath@gmail.comm>
2025-02-11 22:45:55 +0530

    static array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,17 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const users = [
+    { id: 1, name: "Cook", age: 26, skill: "MySQL", designation: "Web Developer", address: "Paris" },
+    { id: 2, name: "David", age: 28, skill: "PHP", designation: "Web Developer", address: "London" },
+    { id: 3, name: "Nathan", age: 28, skill: "PHP", designation: "Web Developer", address: "London" },
+    { id: 4, name: "William", age: 23, skill: "MySQL", designation: "Web Developer", address: "Sydney" }
+  ];
+  const user1 = users.find(user => user.id === 1);
+  const user2 = users.find(user => user.id === 2);
+  const user3 = users.find(user => user.id === 3);
+  const user4 = users.find(user => user.id === 4);
+  // console.log(user);
   return (
     <>
       <div className="body">
@@ -22,36 +32,37 @@ function App() {
           <th>Actions</th>
         </tr>
         <tr className='row'>
-          <td>Cook</td>
-          <td>26</td>
-          <td>MySQL</td>
-          <td>Web Developer</td>
-          <td>Paris</td>
-          <td><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
+          <td>{user1.name}</td>
+          <td>{user1.age}</td>
+          <td>{user1.skill}</td>
+          <td>{user1.designation}</td>
+          <td>{user1.address}</td>
+          <td className='btn'><button className='edit'>Edit</button>
+          <button className='delete'>Delete</button></td>
         </tr>
         <tr className='row'>
-          <td>David</td>
-          <td>28</td>
-          <td>PHP</td>
-          <td>Web Developer</td>
-          <td>London</td>
-          <td><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
+        <td>{user2.name}</td>
+          <td>{user2.age}</td>
+          <td>{user2.skill}</td>
+          <td>{user2.designation}</td>
+          <td>{user2.address}</td>
+          <td className='btn'><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
         </tr>
         <tr className='row'>
-          <td>Nathan</td>
-          <td>28</td>
-          <td>PHP</td>
-          <td>Web Developer</td>
-          <td>London</td>
-          <td><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
+        <td>{user3.name}</td>
+          <td>{user3.age}</td>
+          <td>{user3.skill}</td>
+          <td>{user3.designation}</td>
+          <td>{user3.address}</td>
+          <td className='btn'><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
         </tr>
         <tr className='row'>
-          <td>William</td>
-          <td>23</td>
-          <td>MySQL</td>
-          <td>Web Developer</td>
-          <td>Sydney</td>
-          <td><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
+          <td>{user4.name}</td>
+          <td>{user4.age}</td>
+          <td>{user4.skill}</td>
+          <td>{user4.designation}</td>
+          <td>{user4.address}</td>
+          <td className='btn'><button className='edit'>Edit</button><button className='delete'>Delete</button></td>
         </tr>
       </table>
       </div>
